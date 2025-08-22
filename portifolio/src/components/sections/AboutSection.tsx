08a0c3b8bb83f52fc14d7bc5013304941907f02f
Guilherme Vieira <guilhermearv3@gmail.com>
2025-08-22 19:11:39 -0300

    feat: changing desc

--- portifolio/src/components/sections/AboutSection.tsx
+++ portifolio/src/components/sections/AboutSection.tsx
@@ -10,17 +10,17 @@ export const AboutSection: React.FC = () => {
   const descTraduzida = t(user.desc)
   return (
     <Box component="section" id="about" sx={{ py: { xs: 8, md: 12 }, bgcolor: "#2c2c2c" }}>
-      <Container maxWidth="md">
-        <Stack spacing={3} alignItems="center" textAlign="center">
+      <Container maxWidth="lg">
+        <Stack spacing={3} alignItems="center" textAlign="justify">
           <Avatar
-            alt="Sua foto"
+            alt="Foto de Perfil"
             src={user.img}
-            sx={{ width: 120, height: 120 }}
+            sx={{ width: 260, height: 260 }}
           />
           <Typography variant="h3" component="h2" fontWeight={700} sx={{ color: "#fff", fontSize: { xs: "30px", sm: "39px", md: '48px' } }}>
             {t("sobreSecao.titulo")}
           </Typography>
-          <Typography variant="body1" color="text.secondary" sx={{ color: "#fff" }}>
+          <Typography variant="body1" color="text.secondary" sx={{ color: "#fff", fontSize: { xs: "16px", sm: "18px", md: '20px' } }}>
             {descTraduzida}
           </Typography>
         </Stack>
